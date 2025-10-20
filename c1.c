b61db80e3d737f7aa38db0dcd99fa19b169ede43
BAInc868 <2127674082@qq.com>
2025-10-20 22:27:53 +0800

    cyuyan

---
+++ c1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int d=10,e=5;
+    int f;
+    f=d/e;
+    printf("division=%d\n",f);
+    return 0;
+}
---
+++ c2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a=10,b=5;
+    int c;
+    c=a+b;
+    printf("sum=%d\n",c);
+    return 0;
+}
